f01973773330b8e340a8829980395ca8cec7d72b
Willi4mL <wi.lindblom@gmail.com>
2023-10-27 15:59:37 +0200

    deploy page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {chunkSizeWarningLimit: 1600,}
+  build: {
+    base: 'MovieFlex',
+    chunkSizeWarningLimit: 1600,}
 })
